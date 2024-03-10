756ed8c6ae122fef2998087f2ace2f3278816604
Niget-Kevin <niget.web@gmail.com>
2024-03-10 19:45:24 +0100

    commit pour un test de déploiement

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
-import Home from './Pages/Home/Home';
+import Home from './Pages/Home/Home.jsx';
 import Error from './Pages/Error/Error';
 import Achivement from './Pages/Achivement/Achivement';
 import './App.scss';
@@ -13,12 +13,12 @@ function App() {
   return (
     <React.StrictMode>
       <Router>
-        <div className='main_container'>
-          <main className=''>
+        <div>
+          <main className='main_container'>
             <Header />
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/Achivement/:id" element={<Achivement/>} />    
+              <Route path="/Achivement" element={<Achivement/>} />    
               <Route path="*" element={<Error />} /> 
             </Routes>
           </main>
